5f25f0f105d8164b2bd950ea8ebb863e9f613ae0
Hamdi <waminayatihi@gmail.com>
2023-03-16 15:50:36 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - prints size
+*main - prints sizeof data types
 *Return: 0
 */
 int main(void)
 {
-printf()
-;
+printf("Size of a char:%lu byte(s) \n, Size of an int:%lu byte(s) \n, Size of a long int:%lu byte(s)\n, Size of a long long int:%lu byte(s)\n, Size of a float:%lu byte(s)\n");
 return (0);
 }
