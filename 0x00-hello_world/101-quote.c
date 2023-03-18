a595c5317a6141bbe85c6ce1b2631c25e6e9aa1a
Hamdi <waminayatihi@gmail.com>
2023-03-17 18:18:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main- prints statement
+* main - c program that prints statement
+* Return: 0(Success)
 */
 int main(void)
 {
-system("echo 'and that piece of art is useful" - Dora Korpar, 2015-10-19'");
-return (0)
+fput("\'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (0);
 }
