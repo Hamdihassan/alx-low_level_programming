f218d469f4160004e36189550f5babeed3810b75
Hamdi <waminayatihi@gmail.com>
2023-03-17 17:07:44 -0700

    var

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - prints negative or positive
+ */
+int main(void)
+int(n)
+if {
+print"if the number is greater than 0: is positive"
+}
+elseif{
+print"if the number is 0: is zero"
+}
+{
+else
+{
+print"if the number is less than 0: is negative"
+};
