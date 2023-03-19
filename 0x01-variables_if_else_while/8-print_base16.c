7e0e90478ac1e878b622fbaed81212a136cb98c2
Hamdi <waminayatihi@gmail.com>
2023-03-18 18:03:44 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - program that print base 16 in lowercase
+*Return: 0
+*/
+
+int main(void)
+{
+int i; char ch;
+for (i = 0; i <= 16; i++)
+{
+putchar(i);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
