986a3a12c3c77d4542cca92152b08db943205fe7
Hamdi <waminayatihi@gmail.com>
2023-03-18 17:23:04 -0700

    printnumbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - c program that prints single digit numbers of base 10
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(" %d ", i);
+putchar(i);
+return (0);
+
+}
