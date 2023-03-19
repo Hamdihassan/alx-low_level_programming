aec8ffd0cf807c5d9f930e8992215a779d32404a
Hamdi <waminayatihi@gmail.com>
2023-03-18 18:29:58 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - c program combinations of single-digit numbers
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
