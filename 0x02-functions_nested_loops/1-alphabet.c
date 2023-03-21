690babf403ae6457884438fce97ca013bea2f951
Hamdi <waminayatihi@gmail.com>
2023-03-21 13:47:04 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
 * print_alphabet -  prints the alphabet in lowercase
 * Return: 0
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char alphabets = 'a'
 while (alphabets <= 'z')
 {
 _putchar(alphabets);
-alphabets++;
+alphabets++
 _putchar('\n');
 }
 return (0);
