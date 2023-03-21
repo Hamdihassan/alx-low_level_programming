f2414b95bae3c22a97c49592c097072a55fea465
Hamdi <waminayatihi@gmail.com>
2023-03-21 14:46:11 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints alphabetsx10
+* Return: 0 Always
+*/
+void print_alphabet_x10(void);
+{
+char ch;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (ch = 'a';  ch <= 'z'; ch++)
+
+}
+
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
