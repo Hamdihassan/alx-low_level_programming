1fdf4ed9fab0bcaf78efaee5b25cf960b1604ec9
Hamdi <waminayatihi@gmail.com>
2023-03-27 13:02:07 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ *@str: prints string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
