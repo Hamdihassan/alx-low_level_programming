abb7dfb64f20c31682333c8b48b4a91b7c897924
Hamdi <waminayatihi@gmail.com>
2023-03-27 13:41:24 -0700

    printreverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: character s
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--
+		while (len > 0)
+		{
+			s--;
+			len--;
+		}
+_putchar('\n');
+}
+
