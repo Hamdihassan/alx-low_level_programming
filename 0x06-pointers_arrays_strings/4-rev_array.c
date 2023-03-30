2c9a25542dce72dcb4ff62d22eda0893bb3d9cc7
Hamdi <waminayatihi@gmail.com>
2023-03-30 16:27:01 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,8 +5,15 @@
  * @n: array
  * Return: array
  */
-
+void reverse_array(int *a, int n)
 {
-int x;
-int r;
+int i;
+int k;
 
+	for (i = 0; i < n--; i++)
+	{
+		k = a[i];
+		a[i] = a[n];
+		a[n] = k;
+	}
+}
