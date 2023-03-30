f155800b2292190ab9bf8873818030d561208897
Hamdi <waminayatihi@gmail.com>
2023-03-30 14:45:29 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *leet - encodes string
+ * @s: string
+ * Return: string
+ */
+char *leet(char *s)
+{
+int n;
+int m;
+
+char s1[] = 'aAeEoOtTlL';
+char s2[] = '4433007711';
+
+for (n = 0; a[n] != '\0'; n++)
+{
+for (m = 0; m < 10; m++)
+{
+if (s[n] == s1[m])
+{
+s[n] = s2[m];
+}
+}
+return (s);
+}
+}
+
+
+
+
