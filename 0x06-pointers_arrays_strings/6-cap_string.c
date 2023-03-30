26347fdeeb9d4a70c0c3022cb1ec16f45c2e1644
Hamdi <waminayatihi@gmail.com>
2023-03-30 13:41:14 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *cap_string - capital strings
+ * @s: string
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+int x;
+
+x = 0;
+
+while (!(s[x] >= 'a' && s[x] <= 'z'))
+{
+x++;
+if (s[x - 1] == ' ' ||
+	s[x - 1] == '\t' ||
+	s[x - 1] == '\n' ||
+	s[x - 1] == ',' ||
+	s[x - 1] == ';' ||
+	s[x - 1] == '.' ||
+	s[x - 1] == '!' ||
+	s[x - 1] == '?' ||
+	s[x - 1] == '"' ||
+	s[x - 1] == '(' ||
+	s[x - 1] == ')' ||
+	s[x - 1] == '{' ||
+	s[x - 1] == '}' ||
+	x == 0)
+s[x] = s[x] - 32;
+x++;
+}
+return (s);
+}
