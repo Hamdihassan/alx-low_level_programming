220d06f46fec8cbc9c0e22fe302f1539e3408112
Hamdi <waminayatihi@gmail.com>
2023-04-04 12:47:43 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: string
+ * @b: address of memory to print
+ * @n: size of memory to print
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+
+i = 0;
+
+while (i < n)
+
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_memcpy - a function that copies memory area
+ * @dest: memory location to be copies to
+ * @src: source memory area
+ * @n: unsigned integer value
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+int m;
+
+for (i = 0; i < m; i++;)
+{
+	dest[i] = src[i];
+	n--;
+}
+return (dest);
+
+}
+
