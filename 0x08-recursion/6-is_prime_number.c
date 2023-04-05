39bb8c022e4070c0f5347407ed5be8586403dcd6
Hamdi <waminayatihi@gmail.com>
2023-04-05 16:12:33 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
  * _tmp - helper function that prints prime no.
  * @n: integer
  * @x: integer
- * Return: 0
+ * Return: n
  */
 int _tmp(int n, int x)
 {
@@ -15,21 +15,21 @@ if (n % x == 0 && x > 0)
 {
 	return (0);
 }
-else
-	return (_tmp(n, x - 1));
 
-	}
-/*
+	return (_tmp(n, x - 1));
+}
+
+/**
  * is_prime_number - function that returns 1 if the input int is a prime number
  * @n: integer
- * Return:  return 0
+ * Return:  n
  */
+
 int is_prime_number(int n)
 {
 if (n <= 1)
 {
 	return (0);
 }
-else
 	return (_tmp(n, n - 1));
 }
