6af237f65f9ee8ee0d75a73a4cd3de3f2cbbaece
Hamdi <waminayatihi@gmail.com>
2023-04-09 18:22:20 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]
 {
-if (argc == 1)
-	printf("argv = %s\n", argv[0]);
-
+	if (argc == 1)
+	{
+	printf(" %s\n", argv[0]);
+	}
 	return (0);
 
 }
