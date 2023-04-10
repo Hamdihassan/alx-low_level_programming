26b635b0e67c34e424101895517d08e21fcf0fbb
Hamdi <waminayatihi@gmail.com>
2023-04-09 20:17:59 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
 int main(int argc, char *argv[]
 {
 	if (argc == 1)
