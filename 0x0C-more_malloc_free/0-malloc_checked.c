f9966ab64085e3f25f7be53d375c0c00445ec0fb
Hamdi <waminayatihi@gmail.com>
2023-04-14 16:02:57 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - function checks memory allocation
+ * @b: unsigned int
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int i;
+
+void *ptr = (int *)malloc(n * sizeof(int));
+
+if (i == NULL)
+{
+	exit(98);
+}
+else
+{
+	return (ptr);
+}
+
+}
+
+
