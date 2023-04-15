f17cc69e7a435bac5e8a204ac59288accc1509fb
Hamdi <waminayatihi@gmail.com>
2023-04-14 18:25:06 -0700

    main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array
+ * @size: size of memory
+ * @c: memory address
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+i = 0;
+
+while (i < size)
+
+}
