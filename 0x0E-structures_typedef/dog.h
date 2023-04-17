cdabeff3ea44613ed06457bdbcfbffc6be4b32da
Hamdi <waminayatihi@gmail.com>
2023-04-17 15:09:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog: structure data type
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ *
+ * Description: typedef struct dog
+ * Return: 0
+ */
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+
+
+
+
+
+#endif
