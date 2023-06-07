31db6f9e37f2ebd27e7a3ca26303d559405bbcc6
Hamdi hassan <waminayatihi@gmail.com>
2023-06-07 15:24:43 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: character
+ * return: 0
+ */
+void _puts_recursion(char *s)
+{
+	char n;
+	{
+	putchar("%s");
+	}
+	putchar('\n');
+
+	return (0);
+}
+
