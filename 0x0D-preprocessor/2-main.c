f020cac0f3a21af96806cbae077902febd63da8a
Hamdi <waminayatihi@gmail.com>
2023-06-20 12:45:04 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,14 @@
-#ifndef pi_h
-
 #include <stdio.h>
-#define MYFILE
+#define _MYFILE_
 
 /**
  * main - main function
- *
- * return: 0 always
+ * Description: a program that prints the name of the file
+ * it was compiled from, followed by a new line.
+ * Return: Always 0
  */
-
 int main(void)
 {
-	printf("the name of the file is: %s\n", MYFILE);
+	printf("the name of the file is: %s\n", _MYFILE_);
 	return (0);
 }
