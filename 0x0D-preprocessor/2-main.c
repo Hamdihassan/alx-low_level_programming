1f849e7747ef06ca1ae66cc71efff803a067a25f
Hamdi <waminayatihi@gmail.com>
2023-06-20 11:14:20 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#ifndef pi_h
+
+#include <stdio.h>
+#define MYFILE
+
+/**
+ * main - main function
+ *
+ * return: 0 always
+ */
+
+int main(void)
+{
+	printf("the name of the file is: %s\n", MYFILE);
+	return (0);
+}
