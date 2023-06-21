c64f3c420bf7ce3798295975f6c3d987ea44b0b2
Hamdi <waminayatihi@gmail.com>
2023-06-20 20:29:55 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,15 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-dog_t;
+struct dog dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
