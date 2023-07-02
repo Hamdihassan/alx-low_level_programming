6889d10038a2b42f04227242e79ef24b9c52418b
Hamdi <waminayatihi@gmail.com>
2023-07-01 18:11:45 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,8 +11,5 @@ void calledFirst(void) __attribute__ ((constructor));
 {
 	printf("You're beat! and yet, you must allow,\\n");
 	printf("I bore my house upon my back!\\n");
-}
-	int main(void)
-{
-	return (0);
+	return;
 }
